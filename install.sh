e0273d96322ea61806338d5aadb0e8b47c64b6d8
xBeeegsone <begsOne38@gmail.com>
2017-12-30 17:11:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-apt-get install git sudo
+apt-get install sudo
 sudo apt-get install -y nodejs
-git https://github.com/UnityMCPE/UMProxy.git
 cd UMProxy
 npm install object-inspect
 node .\app.js
